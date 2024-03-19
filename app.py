0dad7cce05f9dc31bbd1d74eaa38595009c85689
Robert Howell <v-robehowell@microsoft.com>
2024-03-19 10:53:49 -0500

    rename runserver.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from web_App_Python import app
 if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', 'localhost')
     try:
-        PORT = int(environ.get('SERVER_PORT', '8000'))
+        PORT = int(environ.get('SERVER_PORT', '7000'))
     except ValueError:
         PORT = 5555
     app.run(HOST, PORT, debug=False)
